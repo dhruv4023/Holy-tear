7be93ffddcc20f9e2614849b54cf0962b67f1e80
Dhruv <dhruv20345@gmail.com>
2022-03-02 23:28:55 +0530

    changes in container and navbar

--- JS/navBar.js
+++ JS/navBar.js
@@ -1,37 +1,70 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar burger */
+// navBar
 var navBar = document.getElementById("navBar");
+var burger = document.getElementById("burger");
+// menues
 var home = document.getElementById("Home");
 var about = document.getElementById("About");
 var contact = document.getElementById("Contact");
-var loader = document.getElementById("load");
+// animation
+// var loader = document.getElementById("load");
+// active menu
+const homeNavItem = document.getElementById("homeNavItem");
+const aboutNavItem = document.getElementById("aboutNavItem");
+const contactNavItem = document.getElementById("contactNavItem");
+
 function navigationBar() {
     console.log(navBar);
     if (navBar.style.display === "flex") {
-        navBar.style.display = "none";
+        // navBar.style.display = "none";
+        // burger.style.display = "fixed";
     } else {
+        burger.style.display = "none";
         navBar.style.display = "flex";
     }
 }
+
 function homeCnt() {
     console.log(navBar);
     home.style.display = "block";
     about.style.display = "none";
     contact.style.display = "none";
-    loader.style.display = "none";
+    // loader.style.display = "none";
+
+    homeNavItem.classList.add("active")
+    aboutNavItem.classList.remove("active")
+    contactNavItem.classList.remove("active")
+    // i.forEach(ltx => ltx.classList.remove("active"));
+
 }
 function aboutCnt() {
     console.log(navBar);
     home.style.display = "none";
     about.style.display = "block";
     contact.style.display = "none";
+
+    homeNavItem.classList.remove("active")
+    aboutNavItem.classList.add("active")
+    contactNavItem.classList.remove("active")
 }
 function contactCnt() {
     console.log(navBar);
     home.style.display = "none";
     about.style.display = "none";
     contact.style.display = "block";
+
+    homeNavItem.classList.remove("active")
+    aboutNavItem.classList.remove("active")
+    contactNavItem.classList.add("active")
 }
-function hideNavigationBar(){
-    navBar.style.display = "none";    
+function hideNavigationBar() {
+    burger.style.display = "block";
+    navBar.style.display = "none";
 }
 
+
+
+
+function activemenu() {
+    i[len].classList.add("active");
+}
